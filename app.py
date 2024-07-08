de6d78cd4adf6e2763c5d37c19fef733dee3a8ab
oferpop <131146809+oferpop@users.noreply.github.com>
2024-07-08 13:14:22 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 CORS(app)
 @app.route('/')
 def hello_world():
-    return {'Hello, World!'}
+    return {"Hello: World!"}
 
 if __name__ == '__main__':
     app.run(debug=True)
